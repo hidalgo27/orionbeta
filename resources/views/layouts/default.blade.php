ee7c5b3b97580d49508ee4d1a447fcf3080891f6
Hidalgo Ch Ponce <hidalgo@MacBook-Pro-de-Hidalgo.local>
2021-03-16 13:35:49 -0500

    Update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -29,7 +29,6 @@
     <link rel="stylesheet" href="{{mix("css/app.css")}}">
     <link rel="stylesheet" href="{{mix("css/plugins.css")}}">
 {{--    <link rel="stylesheet" href="{{mix("css/library.css")}}">--}}
-        <link rel="stylesheet" type="text/css" href="{{asset('vendor/semantic/semantic.min.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 
@@ -170,11 +169,11 @@
 <!-- Footer End -->
 </div>
 <!-- Javascripts -->
-
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
 <script src="{{asset("js/app.js")}}"></script>
 <script src="{{asset("js/plugins.js")}}"></script>
+<link rel="stylesheet" type="text/css" href="{{asset('vendor/semantic/semantic.min.css')}}">
 {{--<script src="/js/jquery-3.3.1.min.js"></script>--}}
 {{--<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>--}}
 {{--<script src="vendor/OwlCarousel/owl.carousel.js"></script>--}}
