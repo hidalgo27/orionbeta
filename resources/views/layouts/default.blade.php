9e1d98ff4375f54c102699a42fe33bb93a5c1310
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-03-25 18:25:00 -0500

    update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -50,21 +50,6 @@
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 {{--    <meta name="theme-color" content="#ffffff">--}}
 
-{{--    <!-- begin olark code -->--}}
-{{--    <script type="text/javascript">--}}
-{{--        ;(function(o,l,a,r,k,y){if(o.olark)return;--}}
-{{--            r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];--}}
-{{--            y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);--}}
-{{--            y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};--}}
-{{--            y.extend=function(i,j){y("extend",i,j)};--}}
-{{--            y.identify=function(i){y("identify",k.i=i)};--}}
-{{--            y.configure=function(i,j){y("configure",i,j);k.c[i]=j};--}}
-{{--            k=y._={s:[],t:[+new Date],c:{},l:a};--}}
-{{--        })(window,document,"static.olark.com/jsclient/loader.js");--}}
-{{--        /* Add configuration calls below this comment */--}}
-{{--        olark.identify('1517-423-10-2650');</script>--}}
-{{--    <!-- end olark code -->--}}
-
 </head>
 
 <body>
@@ -489,5 +474,20 @@
 <script src="{{asset('js/custom.js')}}"></script>
 {{--<script src="js/offset_overlay.js"></script>--}}
 {{--<script src="js/night-mode.js"></script>--}}
+
+<!-- begin olark code -->
+<script type="text/javascript">
+    ;(function(o,l,a,r,k,y){if(o.olark)return;
+        r="script";y=l.createElement(r);r=l.getElementsByTagName(r)[0];
+        y.async=1;y.src="//"+a;r.parentNode.insertBefore(y,r);
+        y=o.olark=function(){k.s.push(arguments);k.t.push(+new Date)};
+        y.extend=function(i,j){y("extend",i,j)};
+        y.identify=function(i){y("identify",k.i=i)};
+        y.configure=function(i,j){y("configure",i,j);k.c[i]=j};
+        k=y._={s:[],t:[+new Date],c:{},l:a};
+    })(window,document,"static.olark.com/jsclient/loader.js");
+    /* Add configuration calls below this comment */
+    olark.identify('1517-423-10-2650');</script>
+<!-- end olark code -->
 </body>
 </html>
