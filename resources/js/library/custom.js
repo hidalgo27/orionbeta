87e61f3379ea75abb6f87c8b05805d2e920760c0
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-04-04 18:42:58 -0500

    Update

--- resources/js/library/custom.js
+++ resources/js/library/custom.js
@@ -385,30 +385,3 @@ $(".product_countdown-timer").each(function(){
 //         }
 //     }
 // })
-
-$('.cate-banners').owlCarousel({
-    loop:true,
-    margin:30,
-    nav:false,
-    dots:false,
-    autoplay:true,
-    autoplayTimeout: 58000,
-    autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:1
-        },
-        1000:{
-            items:1
-        },
-        1200:{
-            items:1
-        },
-        1400:{
-            items:1
-        }
-    }
-})
