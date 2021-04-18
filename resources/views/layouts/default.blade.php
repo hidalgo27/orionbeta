28fd957ddb6376fe7f8af3a86871cb48009e270d
Hidalgo Ch Ponce <hidalgoponce@icloud.com>
2021-04-18 09:45:20 -0500

    Update

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -49,6 +49,15 @@
 {{--    <meta name="msapplication-TileColor" content="#ffffff">--}}
     <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
 {{--    <meta name="theme-color" content="#ffffff">--}}
+<!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4KPNCWLFL1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-4KPNCWLFL1');
+    </script>
 </head>
 
 <body>
