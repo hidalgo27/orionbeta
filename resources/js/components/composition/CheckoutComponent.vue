73ade207e8a3ef0c2050d2f120d490a750dc21d2
Freddy silva <silvahfreddy@gmail.com>
2021-05-22 19:37:03 -0500

    se bajo el pull

--- resources/js/components/composition/CheckoutComponent.vue
+++ resources/js/components/composition/CheckoutComponent.vue
@@ -753,6 +753,7 @@
                                   >Pago Contra entrega</label>
                                 </div>
                               </li>
+
                               <li v-if="false">
                                 <div class="radio-item_1">
                                   <input
